331a83f72cc7e7df16db44ac22f13c9f0eb029e5
TomayevTurxan <tu7hfn0xh@code.edu.az>
2024-03-07 06:01:56 -0800

    tailwind created by Turxan

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      back:"#000000",
+      cardBack:"#121417",
+      textColor:"#FFFFFF",
+      textColor2:"grey",
+      buttonBack1:"#252D37",
+      buttonBack2:"#6A6D71",
+      buttonBack3:"#E68226",
+      buttonSubmit:"#09D9D9",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
